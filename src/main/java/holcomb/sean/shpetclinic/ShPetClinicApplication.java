73f4647a016c5938191df0728e7b79235bc52b98
Sean Holcomb <seanholcomb@gmail.com>
2020-04-30 01:21:40 -0700

    inital commit

---
+++ src/main/java/holcomb/sean/shpetclinic/ShPetClinicApplication.java
@@ -0,0 +1,13 @@
+package holcomb.sean.shpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/holcomb/sean/shpetclinic/ShPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package holcomb.sean.shpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

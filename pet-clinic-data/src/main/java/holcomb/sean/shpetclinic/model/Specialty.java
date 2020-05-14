a8cde780f18fe2457f69079306e7a35f88aeadc3
Sean Holcomb <seanholcomb@gmail.com>
2020-05-13 21:32:52 -0700

    Add specialties, add to vet. closes #27.

---
+++ pet-clinic-data/src/main/java/holcomb/sean/shpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package holcomb.sean.shpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String Description;
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}

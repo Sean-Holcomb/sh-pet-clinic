f0ecf6351491618d520584676866887b707102e5
Sean Holcomb <seanholcomb@gmail.com>
2020-05-24 17:44:07 -0700

    Added Specialties map impl. closes #31.

---
+++ pet-clinic-data/src/main/java/holcomb/sean/shpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package holcomb.sean.shpetclinic.services;
+
+import holcomb.sean.shpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet-clinic-data/src/main/java/holcomb/sean/shpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package holcomb.sean.shpetclinic.services.map;
+
+import holcomb.sean.shpetclinic.model.Specialty;
+import holcomb.sean.shpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

9cd782b3ca8081c0c38219be903305eb91637cc2
Sean Holcomb <seanholcomb@gmail.com>
2020-05-31 14:42:16 -0700

    added spring data JPA for pet. closes #43

---
+++ pet-clinic-data/src/main/java/holcomb/sean/shpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package holcomb.sean.shpetclinic.services.springdatajpa;
+
+import holcomb.sean.shpetclinic.model.Pet;
+import holcomb.sean.shpetclinic.repositories.PetRepository;
+import holcomb.sean.shpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        HashSet<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}

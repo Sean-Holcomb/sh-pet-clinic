73541f04fedd9c59e87c69115fdf056c9bed16a3
Sean Holcomb <seanholcomb@gmail.com>
2020-06-14 23:57:45 -0700

    Add owner details page. Closes #53.

--- pet-clinic-web/src/main/java/holcomb/sean/shpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/holcomb/sean/shpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package holcomb.sean.shpetclinic.controllers;
 import holcomb.sean.shpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,5 +28,13 @@ public class OwnerController {
     public String findOwners(Model model) {
         return "notImplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 
 }

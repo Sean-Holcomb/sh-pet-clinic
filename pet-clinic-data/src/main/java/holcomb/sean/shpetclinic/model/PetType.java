cb50ecb32439e39c918c78d61d3761121211bec8
Sean Holcomb <seanholcomb@gmail.com>
2020-06-17 23:30:35 -0700

    adding builder pattern and toString to petType entitiy. #56

--- pet-clinic-data/src/main/java/holcomb/sean/shpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/holcomb/sean/shpetclinic/model/PetType.java
@@ -10,11 +10,19 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pet_types")
 public class PetType extends BaseEntity {
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
+
+    @Override
+    public String toString() {return name;}
 }
